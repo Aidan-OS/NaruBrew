43b2061fa4081c6b3201578bb432e415d5eca377
Aidan Smith <aidan@aidansmith.dev>
2025-05-31 07:04:18 -0400

    A bunch of stuff that was missed in the initial commit.

---
+++ NaruBrew/src/main/java/quest/dead_end/NaruBrew/NaruBrewApplication.java
@@ -0,0 +1,14 @@
+package quest.dead_end.NaruBrew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaruBrewApplication
+{
+	public static void main(String[] args)
+	{
+		SpringApplication.run(NaruBrewApplication.class, args);
+	}
+
+}
---
+++ NaruBrew/src/test/java/quest/dead_end/NaruBrew/NaruBrewApplicationTests.java
@@ -0,0 +1,13 @@
+package quest.dead_end.NaruBrew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaruBrewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

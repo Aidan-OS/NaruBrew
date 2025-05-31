0bd2b27833fa48779a1d231e183279227a13204c
Aidan Smith <aidan@aidansmith.dev>
2025-05-31 09:49:48 -0400

    Slight edits to tags, initial test for SQL

--- NaruBrew/src/main/java/quest/dead_end/NaruBrew/NaruBrewApplication.java
+++ NaruBrew/src/main/java/quest/dead_end/NaruBrew/NaruBrewApplication.java
@@ -1,7 +1,11 @@
 package quest.dead_end.NaruBrew;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import quest.dead_end.NaruBrew.model.Tag;
+import quest.dead_end.NaruBrew.repository.TagRepository;
 
 @SpringBootApplication
 public class NaruBrewApplication
@@ -9,6 +13,19 @@ public class NaruBrewApplication
 	public static void main(String[] args)
 	{
 		SpringApplication.run(NaruBrewApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner run(TagRepository tagRepo)
+	{
+		return args -> {
+			Tag t = new Tag();
+			t.setName("Test");
+			tagRepo.save(t);
+
+			Tag fetched = tagRepo.findByName("Test");
+			System.out.println("Fetched tag: " + fetched.getName());
+		};
 	}
 
 }
--- NaruBrew/src/main/java/quest/dead_end/NaruBrew/model/Tag.java
+++ NaruBrew/src/main/java/quest/dead_end/NaruBrew/model/Tag.java
@@ -16,4 +16,28 @@ public class Tag
     @ManyToMany(mappedBy = "tags")
     private List<MediaPost> mediaList;
 
+    public Long getId()
+    {
+        return(id);
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getName()
+    {
+        return(this.name);
+    }
+
+    public void setMediaList(List<MediaPost> mediaList)
+    {
+        this.mediaList = mediaList;
+    }
+
+    public List<MediaPost> getMediaList()
+    {
+        return(this.mediaList);
+    }
 }

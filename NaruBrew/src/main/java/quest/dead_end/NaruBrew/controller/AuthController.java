e5104f7bd4d00eb1f44e810aabb43216daaae0b3
Aidan Smith <aidan@aidansmith.dev>
2025-06-01 10:43:19 -0400

    Getting login setup. Swapping to a styles.css, however for some reason it does not preview right with the intellij built in system. Login isn't correctly redirecting, and the page is not taking the CSS with the thymeleaf layout. I have no clue why.

---
+++ NaruBrew/src/main/java/quest/dead_end/NaruBrew/controller/AuthController.java
@@ -0,0 +1,20 @@
+package quest.dead_end.NaruBrew.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AuthController
+{
+    @GetMapping("/login")
+    public String loginPage()
+    {
+        return "login";
+    }
+
+    @GetMapping("/signup")
+    public String signupPage()
+    {
+        return "signup";
+    }
+}
